8bdcd881687e872db3dcf0db803f3479fe5c64fc
JoaquinOnSoft <joaquingp@gmail.com>
2022-02-15 18:58:52 +0100

    New class to read a web page

--- explore-importer-core/src/main/java/com/opentext/explore/importer/http/URLReader.java
+++ explore-importer-core/src/main/java/com/opentext/explore/importer/http/URLReader.java
@@ -1,18 +1,26 @@
 package com.opentext.explore.importer.http;
 
+import java.io.IOException;
+
 import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class PageReader {
+public class URLReader {
 
 	private HttpGet request;
-
-	public PageReader(String ur) {
-
+	
+	protected static final Logger log = LogManager.getLogger(URLReader.class);
+		
+	public URLReader(String url) {		
+        request = new HttpGet(url);
+        log.info("URL: " + url);        
 	}
 
 	public String read() {
@@ -22,21 +30,20 @@ public class PageReader {
 			CloseableHttpResponse response = httpClient.execute(request);
 
 			// Get HttpResponse Status
-			System.out.println(response.getProtocolVersion());              // HTTP/1.1
-			System.out.println(response.getStatusLine().getStatusCode());   // 200
-			System.out.println(response.getStatusLine().getReasonPhrase()); // OK
-			System.out.println(response.getStatusLine().toString());        // HTTP/1.1 200 OK
+			log.info(response.getProtocolVersion());              // HTTP/1.1
+			log.info(response.getStatusLine().getStatusCode());   // 200
+			log.info(response.getStatusLine().getReasonPhrase()); // OK
+			log.info(response.getStatusLine().toString());        // HTTP/1.1 200 OK
 
 			HttpEntity entity = response.getEntity();
 			if (entity != null) {
 				// return it as a String
 				result = EntityUtils.toString(entity);
-				System.out.println(result);
+				log.debug(result);
 			}
-
 		}
-		catch (Exception e) {
-			// TODO: handle exception
+		catch (IOException e) {
+			log.info(" ", e);
 		}
 		
 		return result;
